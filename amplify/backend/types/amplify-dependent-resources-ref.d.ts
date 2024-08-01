43fc534339c7fe7b2209432764a8267f9b39d46f
mohamed <114689440+Mohamedyosry135@users.noreply.github.com>
2024-08-01 19:13:38 +0200

    init

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "lunchcooksc7d46b5c": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
---
+++ android/app/src/main/kotlin/sa/aait/aspbranch/base_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.app.lunchcooks
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
